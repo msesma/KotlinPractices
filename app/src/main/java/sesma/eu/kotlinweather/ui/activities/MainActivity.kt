ae18833ad74279766369d295f6ae83d8c9daa121
Miguel Sesma <miguel@sesma.eu>
2016-02-21 21:49:43 +0100

    chapter 8

--- app/src/main/java/sesma/eu/kotlinweather/ui/activities/MainActivity.kt
+++ app/src/main/java/sesma/eu/kotlinweather/ui/activities/MainActivity.kt
@@ -4,8 +4,13 @@ import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import android.support.v7.widget.LinearLayoutManager
 import android.support.v7.widget.RecyclerView
+import org.jetbrains.anko.async
+import org.jetbrains.anko.find
+import org.jetbrains.anko.longToast
+import org.jetbrains.anko.uiThread
 import sesma.eu.kotlinweather.ui.adapters.ForecastListAdapter
 import sesma.eu.kotlinweather.R
+import sesma.eu.kotlinweather.data.Request
 
 class MainActivity : AppCompatActivity() {
 
@@ -23,8 +28,18 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val forecastList = findViewById(R.id.forecast_list) as RecyclerView
+//        val forecastList = findViewById(R.id.forecast_list) as RecyclerView
+        val forecastList: RecyclerView = find(R.id.forecast_list)
+
         forecastList.layoutManager = LinearLayoutManager(this)
         forecastList.adapter = ForecastListAdapter(items)
+
+        val url = "http://api.openweathermap.org/data/2.5/forecast/daily?" +
+                "APPID=15646a06818f61f7b8d7823ca833e1ce&q=94043&mode=json&units=metric&cnt=7"
+
+        async() {
+            Request(url).run()
+            uiThread { longToast("Request performed") }
+        }
     }
 }
