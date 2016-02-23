dacb92b47316f0d447ac87e8651365d238bb7638
Miguel Sesma <miguel@sesma.eu>
2016-02-23 20:02:43 +0100

    chapter 15

--- app/src/main/java/sesma/eu/kotlinweather/ui/activities/MainActivity.kt
+++ app/src/main/java/sesma/eu/kotlinweather/ui/activities/MainActivity.kt
@@ -3,9 +3,8 @@ package sesma.eu.kotlinweather.ui.activities
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import android.support.v7.widget.LinearLayoutManager
-import android.support.v7.widget.RecyclerView
+import kotlinx.android.synthetic.main.activity_main.*
 import org.jetbrains.anko.async
-import org.jetbrains.anko.find
 import org.jetbrains.anko.toast
 import org.jetbrains.anko.uiThread
 import sesma.eu.kotlinweather.R
@@ -20,7 +19,6 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val forecastList: RecyclerView = find(R.id.forecast_list)
         forecastList.layoutManager = LinearLayoutManager(this)
 
         async() {
