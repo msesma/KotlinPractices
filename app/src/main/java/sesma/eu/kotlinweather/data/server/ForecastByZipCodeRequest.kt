4fa25528868e35df2484654efe6073483c5ea8dd
Miguel Sesma <miguel@sesma.eu>
2016-11-26 18:40:10 +0100

    Chapter 26

--- app/src/main/java/sesma/eu/kotlinweather/data/server/ForecastByZipCodeRequest.kt
+++ app/src/main/java/sesma/eu/kotlinweather/data/server/ForecastByZipCodeRequest.kt
@@ -10,8 +10,8 @@ class ForecastByZipCodeRequest(val zipCode: Long, val gson: Gson = Gson()) {
     companion object {
         private val APP_ID = "15646a06818f61f7b8d7823ca833e1ce"
         private val URL = "http://api.openweathermap.org/data/2.5/forecast/daily?mode=json&units=metric&cnt=7"
-//        private val URL = "http://api.openweathermap.org/data/2.5/forecast/daily?id=3104703&appid=15646a06818f61f7b8d7823ca833e1ce
-        private val COMPLETE_URL = "${URL}&APPID=${APP_ID}&id="
+//        private val COMPLETE_URL = "${URL}&APPID=${APP_ID}&id="
+        private val COMPLETE_URL = "$URL&APPID=$APP_ID&q="
     }
 
     fun execute(): ForecastResult {
--- app/src/main/java/sesma/eu/kotlinweather/ui/activities/MainActivity.kt
+++ app/src/main/java/sesma/eu/kotlinweather/ui/activities/MainActivity.kt
@@ -5,18 +5,21 @@ import android.support.v7.app.AppCompatActivity
 import android.support.v7.widget.LinearLayoutManager
 import android.support.v7.widget.Toolbar
 import kotlinx.android.synthetic.main.activity_main.*
+import org.jetbrains.anko.custom.async
 import org.jetbrains.anko.doAsync
 import org.jetbrains.anko.find
 import org.jetbrains.anko.startActivity
 import org.jetbrains.anko.uiThread
 import sesma.eu.kotlinweather.R
 import sesma.eu.kotlinweather.domain.commands.RequestForecastCommand
+import sesma.eu.kotlinweather.extensions.DelegatesExt
 import sesma.eu.kotlinweather.ui.adapters.ForecastListAdapter
 
 class MainActivity : AppCompatActivity(), ToolbarManager {
 
     //http://openweathermap.org
     override val toolbar by lazy { find<Toolbar>(R.id.toolbar) }
+    val zipCode: Long by DelegatesExt.preference(this, SettingsActivity.ZIP_CODE, SettingsActivity.DEFAULT_ZIP)
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -25,18 +28,24 @@ class MainActivity : AppCompatActivity(), ToolbarManager {
 
         forecastList.layoutManager = LinearLayoutManager(this)
         attachToScroll(forecastList)
-
-        doAsync() {
-            val result = RequestForecastCommand(3104703).execute()
-            uiThread {
-                val adapter = ForecastListAdapter(result) {
-                    startActivity<DetailActivity>(DetailActivity.ID to it.id,
-                            DetailActivity.CITY_NAME to result.city)
-                }
-                forecastList.adapter = adapter
-                toolbarTitle  = "${result.city} (${result.country})"
-            }
-        }
     }
 
+    override fun onResume() {
+        super.onResume()
+        loadForecast()
+    }
+
+    private fun loadForecast() = doAsync {
+        val result = RequestForecastCommand(zipCode).execute()
+//        val result = RequestForecastCommand(3104703).execute()
+
+        uiThread {
+            val adapter = ForecastListAdapter(result) {
+                startActivity<DetailActivity>(DetailActivity.ID to it.id,
+                        DetailActivity.CITY_NAME to result.city)
+            }
+            forecastList.adapter = adapter
+            toolbarTitle = "${result.city} (${result.country})"
+        }
+    }
 }
